a437a8adacf8c42e426f7ebcf7ea9a6d22fb6595
nureddincuneyd <nureddincuneyd@gmail.com>
2020-04-08 11:13:04 +0300

    System Upload
    I was create a system. This first verison

---
+++ footer.php
@@ -0,0 +1,83 @@
+
+
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Covid-19 Monitoring
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020 <a href="https://www.sakusoft.com/">SAKUSoft</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    /* ChartJS
+     * -------
+     * Here we will create a few charts using ChartJS
+     */
+
+    //-------------
+    //- DONUT CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+    var donutData        = {
+      labels: [
+          'Chrome', 
+          'IE',
+          'FireFox', 
+          'Safari', 
+          'Opera', 
+          'Navigator', 
+      ],
+      datasets: [
+        {
+          data: [700,500,400,600,300,100],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var donutChart = new Chart(donutChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions      
+    })
+}
+</script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,52 +1,142 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+<?php include "header.php"; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Home Page</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-<table style="width:100%">
-  <tr>
-  	<th>Country Flag</th>
-    <th>Country Name</th>
-    <th>Active Infected</th>
-    <th>Critical People</th>
-  </tr>  
+    <!-- Main content -->
+    <div class="content">
+        <h5 class="mt-4 mb-2"> All World <code></code></h5>
 
-<?php 
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-info">
+              <span class="info-box-icon"><i class="far fa-bookmark"></i></span>
 
-$fileUrl = "https://corona.lmao.ninja/countries?sort=country";
+              <div class="info-box-content">
+                <span class="info-box-text">Infected People</span>
+                <span class="info-box-number"><?php echo $res["worldCases"]; ?></span>
 
-$data = file_get_contents($fileUrl);
+                <div class="progress" style="visibility: hidden;">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                <span class="progress-description" style="visibility: hidden;">
+                  70% Increase in 30 Days
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-success">
+              <span class="info-box-icon"><i class="far fa-thumbs-up"></i></span>
 
-$dataArray = json_decode($data, true);
+              <div class="info-box-content">
+                <span class="info-box-text">Recovered</span>
+                <span class="info-box-number"><?php echo $res["worldRecovered"]; ?></span>
 
-foreach ($dataArray as $row) {
-	echo "<tr>";
+                <div class="progress" style="visibility: hidden;">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                <span class="progress-description" style="visibility: hidden;">
+                  70% Increase in 30 Days
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-warning">
+              <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
 
-	echo "<td>";
-	?> 
-	<img src='<?php echo $row["countryInfo"]["flag"]; ?>' alt="" width="120" height="70">
-	<?php
-	echo "</td>";
+              <div class="info-box-content">
+                <span class="info-box-text">Tests</span>
+                <span class="info-box-number"><?php echo $res["worldTests"]; ?></span>
 
-	echo "<td>";
-	echo $row["country"];
-	echo "</td>";
+                <div class="progress" style="visibility: hidden;">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                <span class="progress-description" style="visibility: hidden;">
+                  70% Increase in 30 Days
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-danger">
+              <span class="info-box-icon"><i class="fas fa-comments"></i></span>
 
-	echo "<td>";
-	echo $row["active"];
-	echo "</td>";
+              <div class="info-box-content">
+                <span class="info-box-text">Deaths</span>
+                <span class="info-box-number"><?php echo $res["worldDeaths"]; ?></span>
 
-	echo "<td>";
-	echo $row["critical"];
-	echo "</td>";
+                <div class="progress" style="visibility: hidden;">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                <span class="progress-description" style="visibility: hidden;">
+                  70% Increase in 30 Days
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
 
-	echo "</tr>";
+    </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
 
-}
+            <!-- DONUT CHART -->
+            <div class="card card-danger">
+              <div class="card-header">
+                <h3 class="card-title">Donut Chart</h3>
 
- ?>
-</table>
-</body>
-</html>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+        </div>
+      </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+
+<?php include "footer.php"; ?>
